f6c6fc2110599028a870139927bd180ed5a9a88a
HyunDaHyun <gusekgus4746@naver.com>
2022-11-13 16:24:47 +0900

    axios url 변경

--- front/js/script.js
+++ front/js/script.js
@@ -19,7 +19,7 @@ async function getDataSet(category) {
     }
     const dataSet = await axios({
         method: "get", // http method
-        url: `http://gusekgus4746.dothome.co.kr/Map/front/index.html/restaurants?category=${qs}`,
+        url: `http://15.165.214.206:3000/restaurants?category=${qs}`,
         headers: {}, // packet header
         data: {}, // packet body
     });
